34f1387d8fc6c5cdff8f9e334321998143f884ce
Sarith <sarith@sarith.in>
2022-05-25 16:59:51 +0200

    feat(Filter Selection): 1. Added a component for filtering selection.

---
+++ src/PhotoRad_FilterPlantSelection.py
@@ -0,0 +1,55 @@
+"""Performing filtering of plants based on DLI levels.
+    Inputs:
+        _dliData: The output from the dliData component.
+        _dliFilterResult: The dliFilterResult output from the PhotoRad
+        AnalyzePlantSelection component.
+    Output:
+        allOptions: Generates an indexed list of all the combinations(without
+        repetition) of the plants.
+        selection: Provides the selection made for a specific grid/space.
+        selectionGrid: Provides a list of indicies corresponding to the selection
+        made. This can be used to visualize the results through a mesh.
+"""
+
+from __future__ import division
+
+ghenv.Component.Name = "PhotoRad_FilterPlantSelection"
+ghenv.Component.NickName = 'FilterPlantSelection'
+ghenv.Component.Message = 'VER 0.0.04\nMay_25_2022'
+ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.icon
+ghenv.Component.Category = "PhotoRad"
+ghenv.Component.SubCategory = "3 | Analysis"
+try:
+    ghenv.Component.AdditionalHelpFromDocStrings = "2"
+except:
+    pass
+
+__author__ = "SarithS"
+__version__ = "2022.05.25"
+
+import rhinoscriptsyntax as rs
+import itertools
+
+
+def selectPlants(dliFilterResult, dliData):
+    plantList = sorted(set([val.plantInstance.name for val in dliFilterResult]))
+    combinations = list(itertools.chain(
+        *[list(itertools.combinations(plantList, len(plantList) - idx)) for \
+          idx, plant in enumerate(plantList)])) + [()]
+    comboWithID = list(enumerate(combinations))
+    plantDLICombo = [val for val in dliFilterResult if val.dliData == dliData]
+    selection = tuple(
+        sorted([val.plantInstance.name for val in plantDLICombo if val.selection]))
+    selectionIndex = combinations.index(selection)
+    gridSize = dliData.dataSize[0]
+    selectionGrid = [selectionIndex] * gridSize
+
+    return {'allOptions': comboWithID, 'selection': selection,
+            'selectionGrid': selectionGrid}
+
+
+if _dliFilterResult and _dliData:
+    results = selectPlants(_dliFilterResult, _dliData)
+    allOptions = results['allOptions']
+    selection = results['selection']
+    selectionGrid = results['selectionGrid']
