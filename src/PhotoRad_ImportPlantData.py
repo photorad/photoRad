95dcc3a3360a9193a73308c4c9476cfe7a99bd6d
Sarith <sarith@sarith.in>
2022-06-02 14:03:12 +0200

    refactor(version): 1. Updated version.

--- src/PhotoRad_ImportPlantData.py
+++ src/PhotoRad_ImportPlantData.py
@@ -5,18 +5,17 @@
     Output:
         plantDataList: Multiple instances of PlantData that were imported from the csv file and text panel"""
 
+
 ghenv.Component.Name = "PhotoRad_ImportPlantData"
 ghenv.Component.NickName = 'ImportPlantData'
-ghenv.Component.Message = 'VER 0.0.04\nMar_29_2022'
+ghenv.Component.Message = 'VER 0.0.05\nJun_02_2022'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.icon
 ghenv.Component.Category = "PhotoRad"
 ghenv.Component.SubCategory = "02 | Plants"
-try:
-    ghenv.Component.AdditionalHelpFromDocStrings = "2"
-except:
-    pass
+try: ghenv.Component.AdditionalHelpFromDocStrings = "2"
+except: pass
 __author__ = "Sarith"
-__version__ = "2022.03.29"
+__version__ = "2022.06.02"
 
 import rhinoscriptsyntax as rs
 import rhinoscriptsyntax as rs
@@ -24,39 +23,36 @@ import csv
 import scriptcontext as sc
 import os
 
-assert "photoRadDict" in sc.sticky, "The core component was not found. Please drag it canvas."
 
+assert "photoRadDict" in sc.sticky,"The core component was not found. Please drag it canvas."
+#Create hardiness zones and temeprature ranges corresponding to USDA
+def main(csvFilePath,plantDataList):
+    PlantData=sc.sticky["photoRadDict"]["plantDataClass"]
 
-# Create hardiness zones and temeprature ranges corresponding to USDA
-def main(csvFilePath, plantDataList):
-    PlantData = sc.sticky["photoRadDict"]["plantDataClass"]
-
-    plantList = []
+    plantList=[]
     if csvFilePath:
-        assert os.path.exists(
-            csvFilePath), "The path for the file %s is not valid" % csvFilePath
-        with open(csvFilePath, "rb") as csvStream:
-            for idx, lines in enumerate(csvStream):
+        assert os.path.exists(csvFilePath),"The path for the file %s is not valid"%csvFilePath
+        with open(csvFilePath,"rb") as csvStream:
+            for idx,lines in enumerate(csvStream):
                 if idx:
                     if lines.strip():
-                        lineList = lines.strip().split(",")
+                        lineList=lines.strip().split(",")
                         if lineList[0]:
-                            plantInstance = PlantData(*lines.strip().split(","))
+                            plantInstance=PlantData(*lines.strip().split(","))
                             plantList.append(plantInstance)
-                            print("Loaded %s from the file %s" % (
-                            plantInstance, csvFilePath))
+                            print("Loaded %s from the file %s"%(plantInstance,csvFilePath))
 
     if plantDataList:
-        for lines in plantDataList.split("\n"):
-            if lines.strip():
-                lineList = lines.strip().split(",")
-                if lineList[0]:
-                    plantInstance = PlantData(*lineList)
-                    plantList.append(plantInstance)
-                    print("Loaded %s from the intput _plantDataList_" % (plantInstance))
+            for lines in plantDataList.split("\n"):
+                if lines.strip():
+                    lineList=lines.strip().split(",")
+                    if lineList[0]:
+
+                        plantInstance=PlantData(*lineList)
+                        plantList.append(plantInstance)
+                        print("Loaded %s from the intput _plantDataList_"%(plantInstance))
+
 
     return plantList
-
-
 if _plantDataCsv_ or _plantDataList_:
-    plantDataList = main(_plantDataCsv_, _plantDataList_)
+    plantDataList=main(_plantDataCsv_,_plantDataList_)
