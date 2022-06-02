cc2608305c233d3ef18e7e3c72a8d10a34271fcd
Sarith <sarith@sarith.in>
2022-06-02 13:55:52 +0200

    refactor(ladybug, version): 1. Removed the dependency for ladybug folder. 2. Updated version number.

--- src/PhotoRad_ExtractLocationData.py
+++ src/PhotoRad_ExtractLocationData.py
@@ -1,26 +1,24 @@
 """Provides a scripting component.
     Args:
         _epwFile: The absolute file-path for the epw file.
+        _soilDataFile: The absolute file-path for the json file containing soil data. If this file is not available locally, it can be downloaded from https://github.com/sariths/photoRad/raw/main/data/soilData.json
     Returns:
-        locationData: A class containing the details of the location, it's soil type
-        and photoperiod.
-        locationDailyPhotoperiod: Photoperiod for 365 days of the year. Photoperiod is
-        for the number of hours for which visible light is present.
+        locationData: A class containing the details of the location, it's soil type and photoperiod.
+        locationDailyPhotoperiod: Photoperiod for 365 days of the year. Photoperiod is for the number of hours for which visible light is present.
 
 """
 
 ghenv.Component.NickName = 'ExtractLocationData'
-ghenv.Component.Message = 'VER 0.0.04\nMar_29_2022'
+ghenv.Component.Message = 'VER 0.0.05\nJun_02_2022'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.icon
 ghenv.Component.Category = "PhotoRad"
 ghenv.Component.SubCategory = "0 | PhotoRad"
-try:
-    ghenv.Component.AdditionalHelpFromDocStrings = "2"
-except:
-    pass
+try: ghenv.Component.AdditionalHelpFromDocStrings = "2"
+except: pass
+
 
 __author__ = "Sarith"
-__version__ = "2022.03.29"
+__version__ = "2022.02.06"
 
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
@@ -32,161 +30,149 @@ import calendar
 import statistics
 
 
+
+
+
 class LocationData(object):
     """Instantiate a class  """
 
-    def __init__(self, epwFilePath, soilDataLBrelPath="soilData.json"):
+    def __init__(self,epwFilePath,soilDataAbsPath):
 
-        epwSourceDict = self._retrieveEPWdata(epwFilePath)
-        soilDataDict = self._acquireSoilData(soilDataLBrelPath=soilDataLBrelPath)
-        locationSoilDataDict = self._calcLocationSoilData(soilDataDict, epwSourceDict)
+        epwSourceDict=self._retrieveEPWdata(epwFilePath)
+        soilDataDict=self._acquireSoilData(soilDataAbsPath=soilDataAbsPath)
+        locationSoilDataDict=self._calcLocationSoilData(soilDataDict,epwSourceDict)
 
-        self.sourceFile = epwFilePath
-        self.soilData = soilDataDict
-        self.longitude = epwSourceDict["longitude"]
-        self.latitude = epwSourceDict["latitude"]
-        self.location = epwSourceDict["location"]
-        self.difRadData = epwSourceDict["difRadData"]
-        self.hardinessZone = locationSoilDataDict["zone"]
-        self.tmin = locationSoilDataDict["tmin"]
-        self.tmax = locationSoilDataDict["tmax"]
-        self.zipMatch = locationSoilDataDict["zipMatch"]
-        self.longitudeMatch = locationSoilDataDict["lonMatch"]
-        self.latitudeMatch = locationSoilDataDict["latMatch"]
+        self.sourceFile=epwFilePath
+        self.soilData=soilDataDict
+        self.longitude=epwSourceDict["longitude"]
+        self.latitude=epwSourceDict["latitude"]
+        self.location=epwSourceDict["location"]
+        self.difRadData=epwSourceDict["difRadData"]
+        self.hardinessZone=locationSoilDataDict["zone"]
+        self.tmin=locationSoilDataDict["tmin"]
+        self.tmax=locationSoilDataDict["tmax"]
+        self.zipMatch=locationSoilDataDict["zipMatch"]
+        self.longitudeMatch=locationSoilDataDict["lonMatch"]
+        self.latitudeMatch=locationSoilDataDict["latMatch"]
 
     @property
     def sourceFile(self):
         return self._sourceFile
 
     @sourceFile.setter
-    def sourceFile(self, epwPath):
-        assert os.path.exists(
-            epwPath), "The path provided for the epw file (%s) is not valid" % epwPath
-        self._sourceFile = epwPath
+    def sourceFile(self,epwPath):
+        assert os.path.exists(epwPath),"The path provided for the epw file (%s) is not valid"%epwPath
+        self._sourceFile=epwPath
 
-    def _calcLocationSoilData(self, soilDict, epwDict):
 
-        lon = epwDict["longitude"]
-        lat = epwDict["latitude"]
+    def _calcLocationSoilData(self,soilDict,epwDict):
 
-        maxLonLatDiff = 99999
+        lon=epwDict["longitude"]
+        lat=epwDict["latitude"]
 
-        hardinessZone = None
-        hardinessTmin = None
-        hardinessTmax = None
-        hardinessZip = None
-        for key, values in soilDict.items():
+        maxLonLatDiff=99999
+
+        hardinessZone=None
+        hardinessTmin=None
+        hardinessTmax=None
+        hardinessZip=None
+        for key,values in soilDict.items():
             try:
-                currLon = values["lon"]
-                currLat = values["lat"]
-                lonDiff = abs(currLon - lon)
-                latDiff = abs(currLat - lat)
-                lonLat = lonDiff + latDiff
+                currLon=values["lon"]
+                currLat=values["lat"]
+                lonDiff=abs(currLon-lon)
+                latDiff=abs(currLat-lat)
+                lonLat=lonDiff+latDiff
 
-                if lonLat < maxLonLatDiff:
-                    hardinessZone = values["zone"]
-                    hardinessTmin = values["tMin"]
-                    hardinessTmax = values["tMax"]
-                    hardinessZip = key
-                    longitudeMatch = currLon
-                    latitudeMatch = currLat
-                    maxLonLatDiff = lonLat
+                if lonLat<maxLonLatDiff:
+                    hardinessZone=values["zone"]
+                    hardinessTmin=values["tMin"]
+                    hardinessTmax=values["tMax"]
+                    hardinessZip=key
+                    longitudeMatch=currLon
+                    latitudeMatch=currLat
+                    maxLonLatDiff=lonLat
             except KeyError:
                 pass
 
-        return {"zone": hardinessZone, "tmin": hardinessTmin, "tmax": hardinessTmax,
-                "zipMatch": hardinessZip, "lonMatch": longitudeMatch,
-                "latMatch": latitudeMatch}
+        return {"zone":hardinessZone,"tmin":hardinessTmin,"tmax":hardinessTmax,
+                "zipMatch":hardinessZip,"lonMatch":longitudeMatch,
+                "latMatch":latitudeMatch}
 
-    def _acquireSoilData(self, soilDataLBrelPath="soilData.json"):
+
+    def _acquireSoilData(self,soilDataAbsPath):
         """Check if the soil hardiness data exists and retrieve the zip-code based
         values from it."""
 
-        ladybugFolder = sc.sticky["Ladybug_DefaultFolder"]
-        assert os.path.exists(
-            ladybugFolder), "The path for the Ladybug folder(%s) was not found on the " \
-                            "disc" % ladybugFolder
 
-        soilJsonPath = os.path.join(ladybugFolder, soilDataLBrelPath)
+
+
+        soilJsonPath=soilDataAbsPath
 
         if not os.path.exists(soilJsonPath):
-            raise Exception(
-                "The file containing soil data was not found at %s" % soilJsonPath)
+            raise Exception("The file containing soil data was not found at %s"%soilJsonPath)
 
-        soilJsonData = None
+        soilJsonData=None
         with open(soilJsonPath) as soilDataFile:
-            soilJsonData = json.load(soilDataFile)
+            soilJsonData= json.load(soilDataFile)
             if not soilJsonData:
-                raise Exception("The file %s appears to be empty" % soilJsonPath)
+                raise Exception("The file %s appears to be empty"%soilJsonPath)
 
         return soilJsonData
 
-    def _retrieveEPWdata(self, epwFilePath):
+    def _retrieveEPWdata(self,epwFilePath):
 
-        location = None
-        longitude = None
-        latitude = None
+        location=None
+        longitude=None
+        latitude=None
 
-        epwSourceDict = {"location": None, "longitude": None, "latitude": None,
-                         "difRadData": []}
+        epwSourceDict={"location":None,"longitude":None,"latitude":None,"difRadData":[]}
         with open(epwFilePath) as epwData:
             for lines in epwData:
                 if lines.strip():
-                    lineSplit = lines.strip().split(',')
+                    lineSplit=lines.strip().split(',')
                     try:
-                        yearCheck = float(lineSplit[0])
+                        yearCheck=float(lineSplit[0])
                         epwSourceDict["difRadData"].append(int(lineSplit[15]))
                     except ValueError:
-                        if lineSplit[0].lower() == "location":
-                            lineSplit[1:4] = [val.replace(" ", "_") for val in
-                                              lineSplit[1:4]]
-                            epwSourceDict["location"] = "%s-%s-%s" % (
-                            lineSplit[1], lineSplit[2], lineSplit[3])
-                            epwSourceDict["longitude"] = round(float(lineSplit[-3]), 3)
-                            epwSourceDict["latitude"] = round(float(lineSplit[-4]), 3)
+                        if lineSplit[0].lower()=="location":
+                            lineSplit[1:4]=[val.replace(" ","_") for val in lineSplit[1:4]]
+                            epwSourceDict["location"]="%s-%s-%s"%(lineSplit[1],lineSplit[2],lineSplit[3])
+                            epwSourceDict["longitude"]=round(float(lineSplit[-3]),3)
+                            epwSourceDict["latitude"]=round(float(lineSplit[-4]),3)
 
         return epwSourceDict
 
+
     @property
     def dailyPhotoPeriod(self):
-        diffRadData = self.difRadData
-        diffRadDataBinDaily = [diffRadData[idx * 24:(idx * 24) + 23] for idx in
-                               range(365)]
-        dailyPhotoPeriod = [len([num for num in dayList if num]) for dayList in
-                            diffRadDataBinDaily]
+        diffRadData=self.difRadData
+        diffRadDataBinDaily=[diffRadData[idx*24:(idx*24)+23] for idx in range(365)]
+        dailyPhotoPeriod=[len([num for num in dayList if num]) for dayList in diffRadDataBinDaily]
 
         return dailyPhotoPeriod
 
     @property
     def monthlyPhotoPeriodAvg(self):
-        dailyPhotoPeriod = self.dailyPhotoPeriod
-        daysInMonths = [calendar.monthrange(2013, monthNum)[-1] for monthNum in
-                        range(1, 13)]
-        daysInMonthSum = [(sum(daysInMonths[:idx]), sum(daysInMonths[:idx + 1])) for idx
-                          in range(12)]
-        monthlyPhotoPeriods = [dailyPhotoPeriod[idx[0]:idx[1]] for idx in daysInMonthSum]
-        monthlyPhotoPeriodAverage = [round(statistics.mean(monthHours), 2) for monthHours
-                                     in monthlyPhotoPeriods]
+        dailyPhotoPeriod=self.dailyPhotoPeriod
+        daysInMonths=[calendar.monthrange(2013,monthNum)[-1] for monthNum in range(1,13) ]
+        daysInMonthSum=[(sum(daysInMonths[:idx]),sum(daysInMonths[:idx+1])) for idx in range(12)]
+        monthlyPhotoPeriods=[dailyPhotoPeriod[idx[0]:idx[1]] for idx in daysInMonthSum]
+        monthlyPhotoPeriodAverage=[round(statistics.mean(monthHours),2) for monthHours in monthlyPhotoPeriods]
         return monthlyPhotoPeriodAverage
 
     def ToString(self):
         """Placeholder for description about """
-        summaryList = ["Geographical and Soil Data for %s\n" % self.location]
-        summaryList.append(
-            "\tLatitude:%0.2f , Longitude: %0.2f" % (self.latitude, self.longitude))
-        summaryList.append("\tHardiness-Zone:%s , Tmin: %0.2f, Tmax: %0.2f" % (
-        self.hardinessZone, self.tmin, self.tmax))
-        summaryList.append("\tAvg Photoperiod(Jan to Dec): (%s)" % (
-            ",".join(["%d" % val for val in self.monthlyPhotoPeriodAvg])))
-        summaryList.append(
-            "\n\nSoil-data matched in Database: Latitude:%0.2f , Longitude: %0.2f, ZipCode:%s" % (
-            self.latitudeMatch, self.longitudeMatch, self.zipMatch))
-        summaryList.append(
-            "Average Photoperiod calculated from diffuse radiation data in EPW file")
+        summaryList=["Geographical and Soil Data for %s\n"%self.location]
+        summaryList.append("\tLatitude:%0.2f , Longitude: %0.2f"%(self.latitude,self.longitude))
+        summaryList.append("\tHardiness-Zone:%s , Tmin: %0.2f, Tmax: %0.2f"%(self.hardinessZone,self.tmin,self.tmax))
+        summaryList.append("\tAvg Photoperiod(Jan to Dec): (%s)"%(",".join(["%d"%val for val in self.monthlyPhotoPeriodAvg])))
+        summaryList.append("\n\nSoil-data matched in Database: Latitude:%0.2f , Longitude: %0.2f, ZipCode:%s"%(self.latitudeMatch,self.longitudeMatch,self.zipMatch))
+        summaryList.append("Average Photoperiod calculated from diffuse radiation data in EPW file")
         return "\n".join(summaryList)
 
 
-if _epwFile:
-    locationData = LocationData(_epwFile)
-    locationDailyPhotoperiod = locationData.dailyPhotoPeriod
+if _epwFile and _soilDataFile:
+    locationData=LocationData(_epwFile,_soilDataFile)
+    locationDailyPhotoperiod=locationData.dailyPhotoPeriod
     print("Connect the output 'locationData' to text panel to view details")
--- src/PhotoRad_PhotoRad.py
+++ src/PhotoRad_PhotoRad.py
@@ -1,5 +1,4 @@
-"""This component contains the core classes and functions for PhotoRad. Drag this to
-the canvas before proceeding.
+"""This component contains the core classes and functions for PhotoRad. Drag this to the canvas before proceeding.
     Inputs:
         _reload_: Set this to True to reload the component.
 
@@ -8,17 +7,15 @@ the canvas before proceeding.
 
 ghenv.Component.Name = "PhotoRad_PhotoRad"
 ghenv.Component.NickName = 'PhotoRad'
-ghenv.Component.Message = 'VER 0.0.04\nMar_29_2022'
+ghenv.Component.Message = 'VER 0.0.05\nJun_02_2022'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.icon
 ghenv.Component.Category = "PhotoRad"
 ghenv.Component.SubCategory = "0 | PhotoRad"
-try:
-    ghenv.Component.AdditionalHelpFromDocStrings = "1"
-except:
-    pass
+try: ghenv.Component.AdditionalHelpFromDocStrings = "1"
+except: pass
 
-__author__ = "Sarith"
-__version__ = "2022.03.29"
+__author__ = "SarithS"
+__version__ = "2022.06.02"
 
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
@@ -26,157 +23,148 @@ import itertools
 import statistics
 import csv
 import warnings
-
-sc.sticky["photoRadDict"] = {}
-
+sc.sticky["photoRadDict"]={}
 
 class PlantData(object):
-    _temps = range(-65, 70, 5)
-    _tempRanges = [(-200, -65)] + [(_temps[idx], _temps[idx + 1]) for idx in
-                                   range(len(_temps) - 1)] + [(65, 200)]
-    _zones = list(
-        itertools.chain(*[["%sa" % zoneId, "%sb" % zoneId] for zoneId in range(14)]))
 
-    def __init__(self, plantName, dliValue, minTemp=None, maxTemp=None, hardZone=None,
-                 photoPeriod=None, growingSeason=None):
-        self.name = plantName
-        self.dliValue = dliValue
-        self.photoPeriod = photoPeriod
+    _temps = range(-65,70,5)
+    _tempRanges=[(-200,-65)]+[(_temps[idx],_temps[idx+1]) for idx in range(len(_temps)-1)]+[(65,200)]
+    _zones=list(itertools.chain(*[["%sa"%zoneId,"%sb"%zoneId] for zoneId in range(14)]))
 
-        minTemp, maxTemp, hardinessZones = self._calcTempHardiness(minTemp, maxTemp,
-                                                                   hardZone)
-        self.minTemp = minTemp
-        self.maxTemp = maxTemp
-        self.hardinessZone = hardinessZones
-        self.growingSeason = growingSeason
+    def __init__(self,plantName,dliValue,minTemp=None,maxTemp=None,hardZone=None,photoPeriod=None,growingSeason=None):
+        self.name=plantName
+        self.dliValue=dliValue
+        self.photoPeriod=photoPeriod
+
+        minTemp,maxTemp,hardinessZones=self._calcTempHardiness(minTemp,maxTemp,hardZone)
+        self.minTemp=minTemp
+        self.maxTemp=maxTemp
+        self.hardinessZone=hardinessZones
+        self.growingSeason=growingSeason
 
     @property
     def dliValue(self):
         return self._dliValueList
 
     @dliValue.setter
-    def dliValue(self, value):
-        originalValue = value
-        try:
-            value = value.strip().split()
-            value = list(sorted(map(float, value)))
-            value = value if (len(value) == 2) else value * 2
+    def dliValue(self,value):
+       originalValue=value
+       try:
+           value=value.strip().split()
+           value=list(sorted(map(float,value)))
+           value=value if (len(value)==2) else value*2
 
-        except (ValueError, AttributeError):
-            value = [float(value)] * 2
+       except (ValueError,AttributeError):
+           value=[float(value)]*2
 
-        for num in value:
-            assert 0 <= num, "The value for dli should be greater than 0. So the input " \
-                             "%s is incorrect" % originalValue
+       for num in value:
+           assert 0<=num , "The value for dli should be greater than 0. So the input %s is incorrect"%originalValue
 
-        self._dliValueList = list(value)
+       self._dliValueList=list(value)
+
+
 
     @property
     def growingSeason(self):
         return self._growingSeasonList
 
     @growingSeason.setter
-    def growingSeason(self, value):
-        originalValue = value
+    def growingSeason(self,value):
+       originalValue=value
 
-        if value:
-            try:
-                value = value.strip().split()
-                value = list(sorted(map(int, value)))
-            except (ValueError, AttributeError):
-                value = [int(value)]
+       if value:
+           try:
+               value=value.strip().split()
+               value=list(sorted(map(int,value)))
+           except (ValueError,AttributeError):
+               value=[int(value)]
 
-            for num in value:
-                assert 1 <= num <= 12, "The value for growing season should be between " \
-                                       "1(January) and 12(December). So the input %s " \
-                                       "is incorrect" % originalValue
-            self._growingSeasonList = value
-        else:
-            self._growingSeasonList = list(range(1, 13))
+           for num in value:
+               assert 1<=num<=12 , "The value for growing season should be between 1(January) and 12(December). So the input %s is incorrect"%originalValue
+           self._growingSeasonList=value
+       else:
+            self._growingSeasonList=list(range(1,13))
 
     @property
     def photoPeriod(self):
         return self._photoPeriod
 
     @photoPeriod.setter
-    def photoPeriod(self, value):
-        originalValue = value
-        value = value if value is not None else 0
+    def photoPeriod(self,value):
+       originalValue=value
+       value=value if value is not None else 0
 
-        try:
-            value = value.strip().split()
-            value = list(map(int, value))
+       try:
+           value=value.strip().split()
+           value=list(map(int,value))
 
-            value = value if (len(value) == 1) else value * 12
+           value=value if (len(value)==1) else value*12
 
-        except (ValueError, AttributeError):
-            value = [int(value)] * 12
+       except (ValueError,AttributeError):
+           value=[int(value)]*12
 
-        for num in value:
-            assert num in range(1,
-                                25), "The value for photoPeriod should not exceed 24 " \
-                                     "hours. So the input %s is incorrect" % \
-                                     originalValue
 
-        self._photoPeriod = list(value)
+       for num in value:
+           assert num in range(1,25), "The value for photoPeriod should not exceed 24 hours. So the input %s is incorrect"%originalValue
 
-    def _calcTempHardiness(self, minTemp, maxTemp, hardiness):
+       self._photoPeriod=list(value)
 
-        if not any((minTemp, maxTemp, hardiness)):
-            return (
-            PlantData._tempRanges[0][0], PlantData._tempRanges[-1][-1], PlantData._zones)
+
+    def _calcTempHardiness(self,minTemp,maxTemp,hardiness):
+
+
+
+        if not any((minTemp,maxTemp,hardiness)):
+            return (PlantData._tempRanges[0][0],PlantData._tempRanges[-1][-1],PlantData._zones)
 
         if (minTemp or maxTemp) and hardiness:
-            print(
-                "For the plant '%s', it appears that minTemp(%s),maxTemp(%s) and "
-                "hardiness(%s) values have been provided" \
-                ".\n\tThe values of minTemp and maxTemp will be used to set hardiness "
-                "and the provided value will be overridden" % (
-                self.name, minTemp, maxTemp, hardiness))
+
+            print("For the plant '%s', it appears that minTemp(%s),maxTemp(%s) and hardiness(%s) values have been provided"\
+            ".\n\tThe values of minTemp and maxTemp will be used to set hardiness and the provided value will be overridden"%(self.name,minTemp,maxTemp,hardiness))
+
 
         if (minTemp or maxTemp):
-            minTemp = minTemp if minTemp is not None else maxTemp
-            maxTemp = maxTemp if maxTemp is not None else minTemp
-            minTemp = float(minTemp)
-            maxTemp = float(maxTemp)
-            startZoneIdx = None
-            endZoneIdx = None
-            for idx, (minVal, maxVal) in enumerate(PlantData._tempRanges):
-                if minVal <= minTemp <= maxVal:
-                    startZoneIdx = idx
-                if minVal <= maxTemp <= maxVal:
-                    endZoneIdx = idx
+            minTemp=minTemp if minTemp is not None else maxTemp
+            maxTemp=maxTemp if maxTemp is not None else minTemp
+            minTemp=float(minTemp)
+            maxTemp=float(maxTemp)
+            startZoneIdx=None
+            endZoneIdx=None
+            for idx,(minVal,maxVal) in enumerate(PlantData._tempRanges):
+                if minVal<=minTemp<=maxVal:
+                    startZoneIdx=idx
+                if minVal<=maxTemp<=maxVal:
+                    endZoneIdx=idx
 
-            if startZoneIdx == endZoneIdx:
-                endZoneIdx += 1
+            if startZoneIdx==endZoneIdx:
+                endZoneIdx+=1
 
-            return minTemp, maxTemp, PlantData._zones[startZoneIdx:endZoneIdx]
+
+            return minTemp,maxTemp,PlantData._zones[startZoneIdx:endZoneIdx]
         elif hardiness:
 
-            hardSplit = hardiness.split()
+            hardSplit=hardiness.split()
 
-            hardSplit2 = sorted([value for value in hardSplit if len(value) == 2])
-            hardSplit3 = sorted([value for value in hardSplit if len(value) == 3])
-            hardSort = hardSplit2 + hardSplit3
+            hardSplit2=sorted([value for value in hardSplit if len(value)==2])
+            hardSplit3=sorted([value for value in hardSplit if len(value)==3])
+            hardSort=hardSplit2+hardSplit3
+
 
             for hardinessValue in hardSort:
-                assert hardinessValue in PlantData._zones, "The value for hardiness(" \
-                                                           "%s) should be one among " \
-                                                           "%s" % (
-                hardinessValue, ",".join(PlantData._zones))
+                assert hardinessValue in PlantData._zones,"The value for hardiness(%s) should be one among %s"%(hardinessValue,",".join(PlantData._zones))
 
-            if len(hardSort) == 1:
-                hardinessIndex = PlantData._zones.index(hardSort[0])
-                minTemp, maxTemp = PlantData._tempRanges[hardinessIndex]
+            if len(hardSort)==1:
+                hardinessIndex=PlantData._zones.index(hardSort[0])
+                minTemp,maxTemp=PlantData._tempRanges[hardinessIndex]
 
             else:
-                hardinessIndexLow = PlantData._zones.index(hardSort[0])
-                hardinessIndexUp = PlantData._zones.index(hardSort[-1])
-                minTemp = PlantData._tempRanges[hardinessIndexLow][0]
-                maxTemp = PlantData._tempRanges[hardinessIndexUp][1]
+                hardinessIndexLow=PlantData._zones.index(hardSort[0])
+                hardinessIndexUp=PlantData._zones.index(hardSort[-1])
+                minTemp=PlantData._tempRanges[hardinessIndexLow][0]
+                maxTemp=PlantData._tempRanges[hardinessIndexUp][1]
 
-            print(hardSort, self.name)
-            return minTemp, maxTemp, hardSort
+            print(hardSort,self.name)
+            return minTemp,maxTemp,hardSort
 
     @property
     def dliAvg(self):
@@ -203,45 +191,42 @@ class PlantData(object):
         return min(self.photoPeriod)
 
     def ToString(self):
-        return "Plant data for %s" % self.name
+        return "Plant data for %s"%self.name
+
 
     @property
     def summary(self):
-        summaryList = ["Plant data summary for '%s'\n" % self.name]
+        summaryList=["Plant data summary for '%s'\n"%self.name]
         try:
-            summaryList.append("\tDLI value(s): %s" % ",".join(map(list, self.dliValue)))
+            summaryList.append("\tDLI value(s): %s"%",".join(map(list,self.dliValue)))
         except TypeError:
-            summaryList.append("\tDLI value(s): %s" % ",".join(map(str, self.dliValue)))
-        summaryList.append(
-            "\tGrowing Season(s): %s" % ",".join(map(str, self.growingSeason)))
+            summaryList.append("\tDLI value(s): %s"%",".join(map(str,self.dliValue)))
+        summaryList.append("\tGrowing Season(s): %s"%",".join(map(str,self.growingSeason)))
 
-        summaryList.append("\tMinimum Temp: %s" % self.minTemp)
-        summaryList.append("\tMaximum Temp: %s" % self.maxTemp)
-        summaryList.append("\tHardiness Zone(s): %s" % ",".join(self.hardinessZone))
+        summaryList.append("\tMinimum Temp: %s"%self.minTemp)
+        summaryList.append("\tMaximum Temp: %s"%self.maxTemp)
+        summaryList.append("\tHardiness Zone(s): %s"%",".join(self.hardinessZone))
         try:
-            summaryList.append(
-                "\tPhotoperiod(s): %s" % ",".join(map(list, self.photoPeriod)))
+            summaryList.append("\tPhotoperiod(s): %s"%",".join(map(list,self.photoPeriod)))
         except TypeError:
-            summaryList.append(
-                "\tPhotoperiod(s): %s" % ",".join(map(str, self.photoPeriod)))
+            summaryList.append("\tPhotoperiod(s): %s"%",".join(map(str,self.photoPeriod)))
 
         return "\n".join(summaryList)
 
     @property
     def csvString(self):
-        dliValue = list(map(str, self.dliValue))
-        csvStringList = [self.name]
+        dliValue =list(map(str,self.dliValue))
+        csvStringList=[self.name]
 
         csvStringList.append(" ".join(dliValue))
         csvStringList.append(str(self.minTemp))
         csvStringList.append(str(self.maxTemp))
-        csvStringList.append(" ".join(map(str, self.hardinessZone)))
-        csvStringList.append(" ".join(map(str, self.photoPeriod)))
-        csvStringList.append(" ".join(map(str, self.growingSeason)))
+        csvStringList.append(" ".join(map(str,self.hardinessZone)))
+        csvStringList.append(" ".join(map(str,self.photoPeriod)))
+        csvStringList.append(" ".join(map(str,self.growingSeason)))
         return ",".join(csvStringList)
 
     def __str__(self):
         return self.ToString()
 
-
-sc.sticky["photoRadDict"]["plantDataClass"] = PlantData
+sc.sticky["photoRadDict"]["plantDataClass"]=PlantData
